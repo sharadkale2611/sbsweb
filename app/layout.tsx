d6b2c2ea40bf421ad1512846133b1268db1a2d89
sharadkale2611 <sharadkale2611@gmail.com>
2026-08-08 11:45:25 +0530

    preloader_removed temporary from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.variable}>
-        <Preloader />
+        {/* <Preloader /> */}
         {children}
       </body>
     </html>
