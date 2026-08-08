7c6a7b08a6250702d6b436ac8c8a792ca3f1a9eb
sharadkale2611 <sharadkale2611@gmail.com>
2026-08-08 11:30:34 +0530

    contact page issue solve

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,10 @@ import CollegeLayout from "@/components/college-layout/CollegeLayout";
 export default function ContactPage() {
   return (
     <CollegeLayout>
-
+      <div>
+        <h1>Contact Us</h1>
+        <p>Contact page content goes here.</p>
+      </div>
 </CollegeLayout>
   );
 }
