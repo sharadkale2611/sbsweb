0638d07b034da297ff322a68f1ea5e1d4e765496
sharadkale2611 <sharadkale2611@gmail.com>
2026-08-10 14:49:21 +0530

    test07

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,42 @@
-import type { Metadata } from "next";
-import "@plugin_css/animate.min.css";
-import "@plugin_css/bootstrap.min.css";
-import "@plugin_css/icofont.css";
-import "@plugin_css/maginific-popup.min.css";
-import "@plugin_css/nice-select.min.css";
-import "@plugin_css/swiper-bundle.min.css";
-import "@plugin_css/uicons.css";
-import "@css/style.css"
-import "./globals.css";
-import Preloader from "@/components/Preloader";
-import { Poppins } from "next/font/google";
+// import type { Metadata } from "next";
+// import "@plugin_css/animate.min.css";
+// import "@plugin_css/bootstrap.min.css";
+// import "@plugin_css/icofont.css";
+// import "@plugin_css/maginific-popup.min.css";
+// import "@plugin_css/nice-select.min.css";
+// import "@plugin_css/swiper-bundle.min.css";
+// import "@plugin_css/uicons.css";
+// import "@css/style.css"
+// import "./globals.css";
+// import Preloader from "@/components/Preloader";
+// import { Poppins } from "next/font/google";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--ed-font-family",
-});
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+//   variable: "--ed-font-family",
+// });
 
-export const metadata: Metadata = {
-  title: "SBES - College of Arts & Commerce",
-  description: "SBES - College of Arts & Commerce",
-};
+// export const metadata: Metadata = {
+//   title: "SBES - College of Arts & Commerce",
+//   description: "SBES - College of Arts & Commerce",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={poppins.variable}>
+//         {/* <Preloader /> */}
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
 
 export default function RootLayout({
   children,
@@ -29,10 +45,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.variable}>
-        {/* <Preloader /> */}
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,42 +1,47 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 
-const NotFound = () => {
-  return (
-    <main>
-      <section className="ed-error section-gap">
-        <div className="container ed-container">
-          <div className="row justify-content-center">
-            <div className="col-lg-8 col-md-8 col-12">
-              <div className="ed-error__inner text-center">
-                <div className="ed-error__img">
-                  <Image
-                    src="/assets/images/error-img.svg"
-                    alt="error-img"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </div>
-                <div className="ed-error__content">
-                  <p className="ed-error__content-text">
-                    Seems like you've landed on a page which has been archived
-                    or removed, let's take you back home?
-                  </p>
-                  <div className="ed-error__btn">
-                    <Link href="/" className="ed-btn">
-                      Go to Homepage
-                      <i className="fi fi-rr-arrow-small-right" />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
-  );
-};
-export default NotFound;
+// const NotFound = () => {
+//   return (
+//     <main>
+//       <section className="ed-error section-gap">
+//         <div className="container ed-container">
+//           <div className="row justify-content-center">
+//             <div className="col-lg-8 col-md-8 col-12">
+//               <div className="ed-error__inner text-center">
+//                 <div className="ed-error__img">
+//                   <Image
+//                     src="/assets/images/error-img.svg"
+//                     alt="error-img"
+//                     width={0}
+//                     height={0}
+//                     sizes="100vw"
+//                     style={{ width: "100%", height: "auto" }}
+//                   />
+//                 </div>
+//                 <div className="ed-error__content">
+//                   <p className="ed-error__content-text">
+//                     Seems like you've landed on a page which has been archived
+//                     or removed, let's take you back home?
+//                   </p>
+//                   <div className="ed-error__btn">
+//                     <Link href="/" className="ed-btn">
+//                       Go to Homepage
+//                       <i className="fi fi-rr-arrow-small-right" />
+//                     </Link>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </main>
+//   );
+// };
+// export default NotFound;
+
+
+export default function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
+}
