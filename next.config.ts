87a6f6290a72b4c410cc873b1b0d666989e3eb4a
sharadkale2611 <sharadkale2611@gmail.com>
2026-08-10 09:13:55 +0530

    test004

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
-import path from "path";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-
-      react: path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
-      next: path.resolve("./node_modules/next"),
-    };
-
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
